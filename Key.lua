c0769c656f929e288c0942920a7117c3cadfd976
LippTz <141412337+LippTz@users.noreply.github.com>
2025-07-09 20:09:27 +0700

    Create Key.lua

---
+++ Key.lua
@@ -0,0 +1,6 @@
+-- ✅ File Key.lua untuk konfigurasi sistem key
+return {
+    CorrectKey = "kreinhub-access",
+    KeyURL = "https://kreinproject.xyz/getkey",
+    GitHubURL = "https://github.com/KreinProject"
+}
